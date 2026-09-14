c6bfe78107c5d510df3c2368751a50f7dbc15afc
bubblevuofficial-ops <saifchishtivuofficial@gmail.com>
2026-09-14 13:41:55 +0500

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  botName: 'Chishti Bot',
+  ownerName: 'Mahfooz Ahmad',
+  ownerNumber: '923327051601',
+  prefix: '.',
+  mode: 'public', // Options: 'public' or 'private'
+  features: {
+    antilink: false,
+    antisticker: false,
+    fileFilter: false,
+    autoreact: false,
+    statusmention: false
+  }
+};
